a23e23f3d31249d3d84610653d2e2ea851dd1093
Gospel Darlington <darlingtongospel@gmail.com>
2024-05-27 14:12:41 +0100

    able to perform authentication

--- src/dma_frontend/src/App.jsx
+++ src/dma_frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import { dma_backend } from 'declarations/dma_backend'
 import Balance from './components/Balance'
 import Faucet from './components/Faucet'
 import Transfer from './components/Transfer'
 import Header from './components/Header'
+import { AuthClient } from '@dfinity/auth-client'
+import { useEffect } from 'react'
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    checkAuth()
+  }, [])
+
+  const checkAuth = async () => {
+    const authClient = await AuthClient.create()
+
+    if (await authClient.isAuthenticated()) {
+      console.log('User authenticated')
+    } else {
+      authClient.login({
+        maxTimeToLive: BigInt(7 * 24 * 60 * 60 * 1000 * 1000 * 1000),
+        onSuccess: async () => {
+          handleAuthenticated(authClient)
+        },
+      })
+    }
+  }
+
+  const handleAuthenticated = async (authClient) => {
+    console.log('Logged in successfully!')
+  }
+
   return (
     <div id="screen">
       <Header />
